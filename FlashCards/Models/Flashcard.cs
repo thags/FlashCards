89f3ed0831b7fca5a799c02af3b0f59a1d6a12a9
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2021-11-16 18:03:56 -0600

    Added models for the flashcards and stack

---
+++ FlashCards/Models/Flashcard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCards.Models
+{
+    class Flashcard
+    {
+        public int Id { get; set; }
+        public string stack { get; set; }
+        public string front { get; set; }
+        public string back { get; set; }
+    }
+}
