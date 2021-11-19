f4425463a6ba4ff97c4e38aee1e62851b5fc6a1d
Tyler Hagstrom <tyler.hagstrom@gmail.com>
2021-11-19 17:23:55 -0600

    🧹 Cleaned up the UI a little bit

--- FlashCards/UserInput.cs
+++ FlashCards/UserInput.cs
@@ -14,11 +14,11 @@ namespace FlashCards
             bool exitProgram = false;
             while (!exitProgram)
             {
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine("0 to exit");
                 Console.WriteLine("S to View, Create, Delete or Rename Stacks");
                 Console.WriteLine("F to View, Create, Delete or Rename Flashcards");
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("-------------------------- \n");
 
                 string choice = GetUserMenuChoice();
                 switch (choice)
@@ -36,6 +36,7 @@ namespace FlashCards
                         break;
                     default:
                         Console.WriteLine("Incorrect input, try again");
+                        WaitForUser();
                         break;
                 }
             }
@@ -46,14 +47,14 @@ namespace FlashCards
             bool exit = false;
             while (!exit)
             {
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine("0 to return to main menu");
                 Console.WriteLine("V to View all Stacks");
                 Console.WriteLine("C to Create a Stack");
                 Console.WriteLine("R to Rename a stack");
                 Console.WriteLine("D to Delete a stack");
 
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("-------------------------- \n");
 
                 string choice = GetUserMenuChoice();
                 switch (choice)
@@ -65,6 +66,7 @@ namespace FlashCards
                     case "V":
                         Console.Clear();
                         TableVisualisationEngine.ViewTable(StackController.GetStacks());
+                        WaitForUser();
                         break;
                     case "C":
                         Console.Clear();
@@ -74,14 +76,18 @@ namespace FlashCards
                             StackController.InsertStack(newStackName);
                             Console.WriteLine("Newly created stack is:");
                             TableVisualisationEngine.ViewTable(StackController.GetStacks(1, "DESC"));
+                            WaitForUser();
                         }
                         else
                         {
                             Console.WriteLine("No new stack created, invalid name input");
+                            WaitForUser();
                         }
                         break;
                     case "R":
                         Console.Clear();
+                        Console.WriteLine("5 Most recent stacks are: \n");
+                        TableVisualisationEngine.ViewTable(StackController.GetStacks(5, "DESC"));
                         bool chosenStackExists = GetCurrentStack(out string stackToUpdate);
                         if (chosenStackExists)
                         {
@@ -94,6 +100,8 @@ namespace FlashCards
                         break;
                     case "D":
                         Console.Clear();
+                        Console.WriteLine("5 Most recent stacks are: \n");
+                        TableVisualisationEngine.ViewTable(StackController.GetStacks(5, "DESC"));
                         chosenStackExists = GetCurrentStack(out string stackToDelete);
                         if (chosenStackExists)
                         {
@@ -104,6 +112,7 @@ namespace FlashCards
                     default:
                         Console.Clear();
                         Console.WriteLine("Incorrect input, try again");
+                        WaitForUser();
                         break;
                 }
             }
@@ -116,7 +125,9 @@ namespace FlashCards
             bool stackCheck = false;
             while (!stackCheck)
             {
-                Console.WriteLine("Choose a stack of flashcards to interact with: ");
+                Console.WriteLine("5 Most recent stacks are: \n");
+                TableVisualisationEngine.ViewTable(StackController.GetStacks(5, "DESC"));
+                Console.WriteLine("\n Choose a stack of flashcards to interact with: ");
                 stackCheck = GetCurrentStack(out currentStackToWorkOn);
                 if (stackCheck)
                 {
@@ -133,16 +144,17 @@ namespace FlashCards
             {
                 //May be better to choose which stack to be within first, and then a new menu
                 //where a user can edit/view/create/delete cards within that stack
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine($"Current working stack: {currentStackToWorkOn} \n");
                 Console.WriteLine("0 to return to main menu");
                 Console.WriteLine("X to change current stack");
                 Console.WriteLine("V to view all Flashcards in stack");
+                Console.WriteLine("A to view X amount of cards in stack");
                 Console.WriteLine("C to Create a Flashcard in current stack");
                 Console.WriteLine("E to Edit a Flashcard");
                 Console.WriteLine("D to Delete a Flashcard");
 
-                Console.WriteLine("--------------------------");
+                Console.WriteLine("-------------------------- \n");
 
                 string choice = GetUserMenuChoice();
                 switch (choice)
@@ -153,31 +165,65 @@ namespace FlashCards
                         break;
                     case "X":
                         Console.Clear();
+                        Console.WriteLine("\n --------------------------");
                         Console.WriteLine("Choose a stack of flashcards to interact with: ");
+                        Console.WriteLine("-------------------------- \n");
                         bool didReturnRealStack = GetCurrentStack(out string potentialStack);
                         if (didReturnRealStack)
                         {
                             currentStackToWorkOn = potentialStack;
                         }
+                        else
+                        {
+                            Console.WriteLine("Inputted stack name does not exist");
+                            WaitForUser();
+                        }
                         break;
                     case "V":
                         Console.Clear();
                         TableVisualisationEngine.ViewTable(FlashcardController.GetAllCardsInStack(currentStackToWorkOn));
-                        //TODO: Input a stack ID or name and view all flashcards of that stack
+                        WaitForUser();
+                        break;
+                    case "A":
+                        Console.Clear();
+                        Console.WriteLine("\n --------------------------");
+                        Console.WriteLine("What number of cards would you like to see?");
+                        Console.WriteLine("-------------------------- \n");
+                        bool isInt = int.TryParse(Console.ReadLine(), out int amount);
+                        if (isInt)
+                        {
+                            TableVisualisationEngine.ViewTable(FlashcardController.GetXCardsInStack(currentStackToWorkOn, amount));
+                            WaitForUser();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inputted value was not a number");
+                            WaitForUser();
+                        }
                         break;
                     case "C":
                         Console.Clear();
                         string frontOfCard = GetFrontFlashCard();
                         string backOfCard = GetBackFlashCard();
                         FlashcardController.CreateFlashCard(currentStackToWorkOn, frontOfCard, backOfCard);
+                        Console.WriteLine("Newly created card is: \n");
+                        TableVisualisationEngine.ViewTable(FlashcardController.GetXCardsInStack(currentStackToWorkOn, 1));
+                        WaitForUser();
                         break;
                     case "E":
                         Console.Clear();
-                        //get an ID of a flashcard to edit (verify that card exists)
                         bool realCard = GetCardId(out int cardId);
                         if (realCard)
                         {
                             FlashCardEditMenu(cardId);
+                            Console.WriteLine("Card after edit is: \n");
+                            TableVisualisationEngine.ViewTable(FlashcardController.GetCardById(cardId));
+                            WaitForUser();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Inputted card ID does not exist");
+                            WaitForUser();
                         }
                         break;
                     case "D":
@@ -187,17 +233,26 @@ namespace FlashCards
                         {
                             FlashcardController.Delete(cardId);
                             Console.WriteLine("Flashcard deleted");
+                            WaitForUser();
                         }
                         break;
 
                     default:
                         Console.Clear();
                         Console.WriteLine("Incorrect input, try again");
+                        WaitForUser();
                         break;
                 }
             }
         }
         private static string GetUserMenuChoice() => Console.ReadLine().ToUpper();
+        private static void WaitForUser()
+        {
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
         private static string RemoveSpecials(string s)
         {
             s = Regex.Replace(s, "[^a-zA-Z0-9' ']", "");
@@ -205,13 +260,21 @@ namespace FlashCards
         }
         private static string GetFrontFlashCard()
         {
+            Console.WriteLine("\n --------------------------");
             Console.WriteLine("Input information for the front of the flashcard");
-            return RemoveSpecials(Console.ReadLine());
+            Console.WriteLine("-------------------------- \n");
+            string input = Console.ReadLine();
+            Console.Clear();
+            return RemoveSpecials(input);
         }
         private static string GetBackFlashCard()
         {
+            Console.WriteLine("\n --------------------------");
             Console.WriteLine("Input information for the back of the flashcard");
-            return RemoveSpecials(Console.ReadLine());
+            Console.WriteLine("-------------------------- \n");
+            string input = Console.ReadLine();
+            Console.Clear();
+            return RemoveSpecials(input);
         }
 
         public static bool GetNewStackName(out string newStackName)
@@ -220,9 +283,13 @@ namespace FlashCards
             bool correctInput = false;
             while (!correctInput)
             {
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine("Input a name for the new stack");
                 Console.WriteLine("Or input 0 to go back");
-                string userChoice = RemoveSpecials(Console.ReadLine());
+                Console.WriteLine("-------------------------- \n");
+                string input = Console.ReadLine();
+                Console.Clear();
+                string userChoice = RemoveSpecials(input);
                 if (userChoice == "0")
                 {
                     break;
@@ -249,9 +316,12 @@ namespace FlashCards
             bool correctInput = false;
             while (!correctInput)
             {
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine("Input a current stack name");
                 Console.WriteLine("Or input 0 to exit input");
-                string userChoice = RemoveSpecials(Console.ReadLine());
+                Console.WriteLine("-------------------------- \n");
+                string input = Console.ReadLine();
+                string userChoice = RemoveSpecials(input);
                 if (userChoice == "0")
                 {
                     stackName = "none";
@@ -280,8 +350,12 @@ namespace FlashCards
             bool correctInput = false;
             while (!correctInput)
             {
+                Console.WriteLine("--------------------------");
                 Console.WriteLine("Input an ID of a stack");
-                correctInput = int.TryParse(Console.ReadLine(), out userInput);
+                Console.WriteLine("-------------------------- \n");
+                string input = Console.ReadLine();
+                Console.Clear();
+                correctInput = int.TryParse(input, out userInput);
                 if (!correctInput)
                 {
                     Console.WriteLine("Incorrect Input, try again");
@@ -296,10 +370,14 @@ namespace FlashCards
             bool realCard = false;
             while (!correctInput && !realCard)
             {
+                Console.WriteLine("\n --------------------------");
                 Console.WriteLine("Input an ID of a flashcard");
                 Console.WriteLine("Or 0 to exit");
+                Console.WriteLine("-------------------------- \n");
+                string input = Console.ReadLine();
+                Console.Clear();
 
-                correctInput = int.TryParse(Console.ReadLine(), out int userInput);
+                correctInput = int.TryParse(input, out int userInput);
                 
                 if (!correctInput)
                 {
@@ -341,11 +419,14 @@ namespace FlashCards
                 Console.Clear();
                 TableVisualisationEngine.ViewTable(FlashcardController.GetCardById(cardId));
                 Console.WriteLine("\n");
+                Console.WriteLine("--------------------------");
                 Console.WriteLine("0 to return to previous menu");
                 Console.WriteLine("F to edit the front of the card");
                 Console.WriteLine("B to edit the back of the card");
+                Console.WriteLine("-------------------------- \n");
 
                 string userChoice = GetUserMenuChoice();
+                Console.Clear();
                 switch (userChoice)
                 {
                     case "0":
